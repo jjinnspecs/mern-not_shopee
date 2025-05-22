70de6961185b835b98f04be3723aed155347fbb9
jjinspecs <j3.cariaso@gmail.com>
2025-05-22 15:29:55 +0800

    fix: update imports

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-import { connectDB } from './config/db.js';
+import { connectDB } from "./config/db.js";
 
-import productRoutes from './routes/product.route.js';
+import productRoutes from "./routes/product.route.js";
 
 
 dotenv.config();
